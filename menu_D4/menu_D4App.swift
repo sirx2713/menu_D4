314e093303e0dfbdb4f5abc3b9a3c6023de3e38c
Sirx <bradx2713@gmail.com>
2024-02-29 03:55:23 +0200

    Initial Commit

---
+++ menu_D4/menu_D4App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D4App.swift
+//  menu_D4
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct menu_D4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
